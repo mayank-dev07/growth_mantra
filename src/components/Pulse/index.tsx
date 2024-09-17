7ef5294f38a5ce589ea4daddf54446e46de80a07
mayank-dev07 <devmayank.hts@gmail.com>
2024-09-17 17:03:57 +0530

    feat: accordian added

--- src/components/Pulse/index.tsx
+++ src/components/Pulse/index.tsx
@@ -2,6 +2,13 @@ import { ArrowRight } from "lucide-react";
 import Image from "next/image";
 import React from "react";
 
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+
 const probeItems = [
   "Market research, Ideal Customer Profile, and competitor analysis",
   "Comprehensive data analysis",
@@ -66,24 +73,33 @@ const Pulse = () => {
               className="object-fill lg:object-left h-full"
             />
           </div>
+
           <div className="relative z-20 flex flex-col w-full justify-center items-center lg:items-end lg:right-[150px] mt-20">
             <div className="h-auto w-10/12 lg:w-[45%] tracking-wide">
               <p className="text-3xl lg:text-5xl font-bold">
                 <span className="text-[#E74C3C]">P</span>&nbsp;-&nbsp;Probe
               </p>
-              <div className="text-lg lg:text-xl flex flex-col w-full shadow-2xl rounded-2xl px-6 py-12 gap-3 bg-white">
-                <p className="leading-normal text-[#E74C3C] font-bold">
-                  We begin by deeply probing your business ecosystem, market
-                  dynamics, and customer behavior. This involves:
-                </p>
-                <ul className="space-y-2 text-base lg:text-lg text-gray-700">
-                  {probeItems.map((item, index) => (
-                    <li key={index} className="flex items-center">
-                      <span className="h-2 w-2 mt-1.5 bg-[#E74C3C] rounded-full flex-shrink-0 mr-3"></span>
-                      <span>{item}</span>
-                    </li>
-                  ))}
-                </ul>
+              <div className="text-lg lg:text-xl flex flex-col w-full shadow-2xl rounded-2xl px-6 py-4 gap-3 bg-white">
+                <Accordion type="single" collapsible>
+                  <AccordionItem value="item-1">
+                    <AccordionTrigger className="hover:no-underline">
+                      <p className="leading-normal text-[#E74C3C] font-bold test-base md:text-lg ">
+                        We begin by deeply probing your business ecosystem,
+                        market dynamics, and customer behavior
+                      </p>
+                    </AccordionTrigger>
+                    <AccordionContent>
+                      <ul className="space-y-2 text-base lg:text-lg text-gray-700">
+                        {probeItems.map((item, index) => (
+                          <li key={index} className="flex items-center">
+                            <span className="h-2 w-2 mt-1.5 bg-[#E74C3C] rounded-full flex-shrink-0 mr-3"></span>
+                            <span>{item}</span>
+                          </li>
+                        ))}
+                      </ul>
+                    </AccordionContent>
+                  </AccordionItem>
+                </Accordion>
               </div>
             </div>
           </div>
@@ -92,86 +108,120 @@ const Pulse = () => {
               <p className="text-3xl lg:text-5xl font-bold">
                 <span className="text-[#E74C3C]">U</span>&nbsp;-&nbsp;Uncover
               </p>
-              <div className="text-lg flex flex-col w-full shadow-2xl rounded-2xl px-6 py-12 gap-3 bg-white">
-                <p className="leading-normal text-[#E74C3C] font-bold">
-                  With insights from our probe, we uncover hidden opportunities
-                  and challenges:
-                </p>
-                <ul className="space-y-2 text-base lg:text-lg text-gray-700">
-                  {uncoverItems.map((item, index) => (
-                    <li key={index} className="flex items-center">
-                      <span className="h-2 w-2 mt-1.5 bg-[#E74C3C] rounded-full flex-shrink-0 mr-3"></span>
-                      <span>{item}</span>
-                    </li>
-                  ))}
-                </ul>
+              <div className="text-lg flex flex-col w-full shadow-2xl rounded-2xl py-4 px-2 md:px-6 md:py-12 bg-white">
+                <Accordion type="single" collapsible>
+                  <AccordionItem value="item-1">
+                    <AccordionTrigger className="hover:no-underline">
+                      <p className="leading-normal text-[#E74C3C] font-bold test-base md:text-lg">
+                        With insights from our probe, we uncover hidden
+                        opportunities and challenges
+                      </p>
+                    </AccordionTrigger>
+                    <AccordionContent>
+                      <ul className="space-y-2 text-base lg:text-lg text-gray-700">
+                        {uncoverItems.map((item, index) => (
+                          <li key={index} className="flex items-center">
+                            <span className="h-2 w-2 mt-1.5 bg-[#E74C3C] rounded-full flex-shrink-0 mr-3"></span>
+                            <span>{item}</span>
+                          </li>
+                        ))}
+                      </ul>
+                    </AccordionContent>
+                  </AccordionItem>
+                </Accordion>
               </div>
             </div>
           </div>
+
           <div className="relative z-20 flex flex-col w-full justify-center items-center lg:items-end lg:right-[120px] mt-20">
             <div className="h-auto w-10/12 lg:w-[45%] tracking-wide">
               <p className="text-3xl lg:text-5xl font-bold">
                 <span className="text-[#E74C3C]">L</span>
                 &nbsp;-&nbsp;Leverage
               </p>
-              <div className="text-lg flex flex-col w-full shadow-2xl rounded-2xl px-6 py-12 gap-3 bg-white">
-                <p className="leading-normal text-[#E74C3C] font-bold">
-                  We leverage our findings to craft tailored strategies: Develop
-                  multi-channel growth plans Customer journey mapping
-                  Identifying key performance indicators (KPIs)
-                </p>
-                <ul className="space-y-2 text-base lg:text-lg text-gray-700">
-                  {leverageItems.map((item, index) => (
-                    <li key={index} className="flex items-center">
-                      <span className="h-2 w-2 mt-1.5 bg-[#E74C3C] rounded-full flex-shrink-0 mr-3"></span>
-                      <span>{item}</span>
-                    </li>
-                  ))}
-                </ul>
-              </div>
-            </div>
-          </div>
-          <div className="relative z-20 flex flex-col w-full justify-center items-center lg:items-end lg:right-[400px] mt-20">
-            <div className="h-auto w-10/12 lg:w-[45%] tracking-wide">
-              <p className="text-3xl lg:text-5xl font-bold">
-                <span className="text-[#E74C3C]">S</span>&nbsp;-&nbsp;Synthesize
-              </p>
-              <div className="text-lg flex flex-col w-full shadow-2xl rounded-2xl px-6 py-12 gap-3 bg-white">
-                <p className="leading-normal text-[#E74C3C] font-bold">
-                  Our approach synthesizes various growth levers for maximum
-                  impact
-                </p>
-                <ul className="space-y-2 text-base lg:text-lg text-gray-700">
-                  {synthesizesItems.map((item, index) => (
-                    <li key={index} className="flex items-center">
-                      <span className="h-2 w-2 mt-1.5 bg-[#E74C3C] rounded-full flex-shrink-0 mr-3"></span>
-                      <span>{item}</span>
-                    </li>
-                  ))}
-                </ul>
+              <div className="text-lg flex flex-col w-full shadow-2xl rounded-2xl py-4 px-2 md:px-6 md:py-12 bg-white">
+                <Accordion type="single" collapsible>
+                  <AccordionItem value="item-1">
+                    <AccordionTrigger className="hover:no-underline !border-b-0">
+                      <p className="leading-normal text-[#E74C3C] font-bold test-base md:text-lg">
+                        We leverage our findings to craft tailored strategies:
+                        Develop multi-channel growth plans Customer journey
+                        mapping Identifying key performance indicators (KPIs)
+                      </p>
+                    </AccordionTrigger>
+                    <AccordionContent>
+                      <ul className="space-y-2 text-base lg:text-lg text-gray-700">
+                        {leverageItems.map((item, index) => (
+                          <li key={index} className="flex items-center">
+                            <span className="h-2 w-2 mt-1.5 bg-[#E74C3C] rounded-full flex-shrink-0 mr-3"></span>
+                            <span>{item}</span>
+                          </li>
+                        ))}
+                      </ul>
+                    </AccordionContent>
+                  </AccordionItem>
+                </Accordion>
               </div>
             </div>
           </div>
 
-          <div className="relative z-20 flex flex-col w-full justify-center items-center lg:items-end lg:right-[650px] mt-20">
+          <div className="relative z-20 flex flex-col w-full justify-center items-center lg:items-end lg:right-[500px] mt-20">
+            <div className="h-auto w-10/12 lg:w-[45%] tracking-wide">
+              <p className="text-3xl lg:text-5xl font-bold">
+                <span className="text-[#E74C3C]">S</span>&nbsp;-&nbsp;Synthesize
+              </p>
+              <div className="text-lg flex flex-col w-full shadow-2xl rounded-2xl py-4 px-2 md:px-6 md:py-12 gap-3 bg-white">
+                <Accordion type="single" collapsible>
+                  <AccordionItem value="item-1">
+                    <AccordionTrigger className="hover:no-underline">
+                      <p className="leading-normal text-[#E74C3C] font-bold test-base md:text-lg">
+                        Our approach synthesizes various growth levers for
+                        maximum impact
+                      </p>
+                    </AccordionTrigger>
+                    <AccordionContent>
+                      <ul className="space-y-2 text-base lg:text-lg text-gray-700">
+                        {synthesizesItems.map((item, index) => (
+                          <li key={index} className="flex items-center">
+                            <span className="h-2 w-2 mt-1.5 bg-[#E74C3C] rounded-full flex-shrink-0 mr-3"></span>
+                            <span>{item}</span>
+                          </li>
+                        ))}
+                      </ul>
+                    </AccordionContent>
+                  </AccordionItem>
+                </Accordion>
+              </div>
+            </div>
+          </div>
+
+          <div className="relative z-20 flex flex-col w-full justify-center items-center lg:items-end lg:right-[700px] mt-20">
             <div className="h-auto w-10/12 lg:w-[45%] tracking-wide">
               <p className="text-3xl lg:text-5xl font-bold">
                 <span className="text-[#E74C3C]">E</span>&nbsp;-&nbsp; Execute &
                 Evolve
               </p>
-              <div className="text-lg flex flex-col w-full shadow-2xl rounded-2xl px-6 py-12 gap-3 bg-white">
-                <p className="leading-normal text-[#E74C3C] font-bold">
-                  {`We don't just strategize; we execute with precision and
+              <div className="text-lg flex flex-col w-full shadow-2xl rounded-2xl py-4 px-2 md:px-6 md:py-12 bg-white">
+                <Accordion type="single" collapsible>
+                  <AccordionItem value="item-1">
+                    <AccordionTrigger className="hover:no-underline">
+                      <p className="leading-normal text-[#E74C3C] font-bold test-base md:text-lg">
+                        {`We don't just strategize; we execute with precision and
                   continuously evolve:`}
-                </p>
-                <ul className="space-y-2 text-base lg:text-lg text-gray-700">
-                  {executeItems.map((item, index) => (
-                    <li key={index} className="flex items-center">
-                      <span className="h-2 w-2 mt-1.5 bg-[#E74C3C] rounded-full flex-shrink-0 mr-3"></span>
-                      <span>{item}</span>
-                    </li>
-                  ))}
-                </ul>
+                      </p>
+                    </AccordionTrigger>
+                    <AccordionContent>
+                      <ul className="space-y-2 text-base lg:text-lg text-gray-700">
+                        {executeItems.map((item, index) => (
+                          <li key={index} className="flex items-center">
+                            <span className="h-2 w-2 mt-1.5 bg-[#E74C3C] rounded-full flex-shrink-0 mr-3"></span>
+                            <span>{item}</span>
+                          </li>
+                        ))}
+                      </ul>
+                    </AccordionContent>
+                  </AccordionItem>
+                </Accordion>
               </div>
             </div>
           </div>
