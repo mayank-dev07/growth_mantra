79a3c76b6368aa131e829be315c501ea08b3dcd1
mayank_dev <123981067+mayank-dev07@users.noreply.github.com>
2024-09-18 12:51:04 +0530

    Update header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -8,7 +8,7 @@ const Header = () => {
         <div className="w-full h-full p-2 bg-[#16213E] flex justify-center items-center text-white gap-2 md:gap-5">
           <div>Low Marketing Budget? We&apos;ve Got You!</div>
           <div className="px-4 py-1 rounded-full bg-[#E74C3C] text-sm md:text-lg">
-            Schedule a call
+            Schedule&nbsp;a&nbsp;call
           </div>
         </div>
         <div className="w-full h-full flex justify-center items-center gap-3 text-sm md:text-lg lg:gap-10 py-4 text-[#6F6F6F]">
