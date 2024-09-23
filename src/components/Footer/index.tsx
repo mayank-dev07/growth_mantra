2ce7eca5f27523904664043cfa93a1e00d1ba47a
mayank-dev07 <devmayank.hts@gmail.com>
2024-09-23 18:12:33 +0530

    feat:drawer added

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -7,7 +7,7 @@ const Footer = () => {
     <footer className="bg-white rounded-lg py-8">
       <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
         <div className="sm:flex sm:items-center sm:justify-between">
-          <div className="flex flex-wrap justify-center items-center mb-6 text-base lg:text-lg  text-gray-500 sm:mb-0 dark:text-gray-400 gap-4 md:gap-10 lg:gap-16">
+          <div className="flex flex-wrap justify-center items-center mb-6 text-sm md:text-base lg:text-lg  text-gray-500 sm:mb-0 dark:text-gray-400 gap-4 md:gap-10 lg:gap-16">
             <Link href="/" className="text-[#E74C3C]">
               About us
             </Link>
@@ -29,12 +29,12 @@ const Footer = () => {
           </div>
         </div>
         <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8 w-full lg:w-10/12" />
-        <div className="w-full flex flex-col lg:flex-row gap-8 justify-between items-center">
-          <span className="block text-lg lg:text-sm text-gray-500 sm:text-center dark:text-gray-400">
+        <div className="w-full flex flex-col lg:flex-row gap-6 justify-between items-center">
+          <span className="block text-base lg:text-sm text-gray-500 sm:text-center dark:text-gray-400">
             © 2022 ABC. All rights reserved.
           </span>
           <Image src="/Frame.png" alt="frame" width={200} height={200} />
-          <ul className="flex flex-wrap items-center mb-6 text-lg lg:text-sm font-bold text-gray-500 sm:mb-0 dark:text-gray-400">
+          <ul className="flex flex-wrap items-center mb-6 text-base lg:text-sm font-bold text-gray-500 sm:mb-0 dark:text-gray-400">
             <li>
               <a href="#" className="hover:underline me-4 md:me-6">
                 About
