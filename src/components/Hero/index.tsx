fa3c2f562210aef47afbd521a72812a012156572
Aryan Kumer <aryandot1@gmail.com>
2024-09-29 12:02:23 +0530

    changes

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -41,7 +41,7 @@ const Hero = () => {
             </div>
             <div className="w-full flex flex-col gap-6 p-1 px-2 lg:px-0 justify-center sm:justify-start items-center sm:items-start">
               <div className="text-5xl sm:text-5xl md:text-5xl lg:text-7xl font-bold flex flex-col gap-4 text-center sm:text-left">
-                Your&nbsp;Retention&nbsp;
+                Your&nbsp;Acquisition&nbsp;
                 <div className="flex items-center gap-2 justify-center sm:justify-start">
                   <div className="border-[#E74C3C] py-2 px-4 md:px-8 text-3xl sm:text-4xl md:text-5xl lg:text-7xl border-4 w-fit rounded-full text-[#E74C3C] font-bold shadow-2xl">
                     Growth
@@ -50,11 +50,11 @@ const Hero = () => {
                 </div>
               </div>
               <div className="text-[#425466] text-base md:text-lg py-4 text-center sm:text-left">
-              Transform One-Time Users into Lifelong Customers
-              </div>
+              Transform Your Customer Acquisition from Guesswork to a Science
+                            </div>
               <div className="w-full flex justify-center md:justify-start">
                 <div className="group rounded-full w-fit px-4 py-2 flex text-sm md:text-base lg:text-xl font-bold text-white bg-[#E74C3C] hover:bg-white hover:text-[#E74C3C] border-[1px] shadow-xl tracking-wider items-center gap-4 cursor-pointer transition-all duration-500 ease-in-out">
-                Boost Your  Retention
+                Boost Your Acquisition
                   <div className="p-3 bg-white rounded-full text-black group-hover:bg-[#E74C3C] group-hover:text-white transition-all duration-300 ease-in-out">
                     <ArrowRight />
                   </div>
@@ -64,7 +64,7 @@ const Hero = () => {
           </div>
         </div>
         <div className="w-full lg:w-4/12 pt-4 lg:pt-12 flex justify-center items-center">
-          <div className="w-full md:w-1/2 lg:w-full flex justify-center items-center">
+          <div className="w-full md:w-1/2 lg:w-full flex justify-center items-center -z-10 md:-z-0">
             <VerticalSwiper />
           </div>
         </div>
