062ca014360dc41b9f2159d56d22fd2e0f2ffc2c
mayank-dev07 <devmayank.hts@gmail.com>
2024-10-08 19:43:06 +0530

    changed

--- src/components/Offering/UiOffering.tsx
+++ src/components/Offering/UiOffering.tsx
@@ -26,7 +26,7 @@ const CheckmarkIcon = () => (
     <Image
       width={30}
       height={30}
-      src="/success.png"
+      src="/assets/success_red.png"
       alt="Checkmark Icon"
       className="w-full h-full object-contain"
     />
