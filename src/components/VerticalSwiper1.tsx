4ad5bedff131cfdbc03e062bd43c8cce896854b6
mayank-dev07 <devmayank.hts@gmail.com>
2024-10-11 02:55:58 +0530

    vertical swiper height changed

--- src/components/VerticalSwiper1.tsx
+++ src/components/VerticalSwiper1.tsx
@@ -22,7 +22,7 @@ export default function VerticalSwiper() {
         allowTouchMove={false}
         loop={true}
         modules={[Autoplay, Pagination]}
-        className="!h-[500px] lg:!min-h-[70vh] w-full [&_.swiper-pagination-bullet-active]:!bg-[#576D2C] [&_.swiper-pagination-bullet]:bg-white [&_.swiper-pagination-bullet]:!border-2 [&_.swiper-pagination-bullet]:!border-[#576D2C] [&_.swiper-pagination-bullet]:!w-5 [&_.swiper-pagination-bullet]:!h-5 [&_.swiper-pagination-bullet]:!p-3"
+        className="!h-[500px] lg:!min-h-[600px] w-full [&_.swiper-pagination-bullet-active]:!bg-[#576D2C] [&_.swiper-pagination-bullet]:bg-white [&_.swiper-pagination-bullet]:!border-2 [&_.swiper-pagination-bullet]:!border-[#576D2C] [&_.swiper-pagination-bullet]:!w-5 [&_.swiper-pagination-bullet]:!h-5 [&_.swiper-pagination-bullet]:!p-3"
       >
         <SwiperSlide>
           <Link href="/ContactUs" passHref>
